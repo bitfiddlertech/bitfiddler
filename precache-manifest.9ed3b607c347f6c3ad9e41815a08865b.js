8e290c7d289cf9a9666b396510ffa0884ada6d12
Brandon Lucas <thebrandonlucas@gmail.com>
2019-05-25 12:55:17 -0400

    Updates

---
+++ precache-manifest.9ed3b607c347f6c3ad9e41815a08865b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42c7de7bce8f44a53e131548eb119ddd",
+    "url": "/bitfiddler/static/media/Bitfiddle.42c7de7b.png"
+  },
+  {
+    "revision": "380bcce88327f4cdc4d5",
+    "url": "/bitfiddler/static/js/runtime~main.451fab37.js"
+  },
+  {
+    "revision": "578fe167459fb5890e4f",
+    "url": "/bitfiddler/static/js/main.e255fdf9.chunk.js"
+  },
+  {
+    "revision": "ab0669508c826028269a",
+    "url": "/bitfiddler/static/js/2.f2d36965.chunk.js"
+  },
+  {
+    "revision": "578fe167459fb5890e4f",
+    "url": "/bitfiddler/static/css/main.c3d596f3.chunk.css"
+  },
+  {
+    "revision": "93505422a89cde28d07381c5d912ab59",
+    "url": "/bitfiddler/index.html"
+  }
+];
