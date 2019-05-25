ce4aa2aeae332db966e0eeca1e613e18b5a77fc4
Brandon Lucas <thebrandonlucas@gmail.com>
2019-05-25 16:01:20 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/public/precache-manifest.f1c4b38b1921a8d183513ce60c27beb3.js"
+  "/bitfiddler/precache-manifest.8a7c3a988fc33b9c67663a92ac7fb9e7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/public/index.html", {
+workbox.routing.registerNavigationRoute("/bitfiddler/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
