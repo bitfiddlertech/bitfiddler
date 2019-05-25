9651b1b92bf40df22213944af72df40a97d57e16
Brandon Lucas <thebrandonlucas@gmail.com>
2019-05-25 15:57:26 -0400

    Updates

---
+++ precache-manifest.f1c4b38b1921a8d183513ce60c27beb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42c7de7bce8f44a53e131548eb119ddd",
+    "url": "/public/static/media/Bitfiddle.42c7de7b.png"
+  },
+  {
+    "revision": "71839442e14b9da6d00a",
+    "url": "/public/static/js/runtime~main.de5d37ed.js"
+  },
+  {
+    "revision": "41368a8b502f1007e4fb",
+    "url": "/public/static/js/main.3137e4f9.chunk.js"
+  },
+  {
+    "revision": "ab0669508c826028269a",
+    "url": "/public/static/js/2.f2d36965.chunk.js"
+  },
+  {
+    "revision": "41368a8b502f1007e4fb",
+    "url": "/public/static/css/main.c3d596f3.chunk.css"
+  },
+  {
+    "revision": "1c9f49adeb2956f559d99945bc610a72",
+    "url": "/public/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.287c0ed49dc66907808eca2c62dff4e8.js"
+  "/public/precache-manifest.f1c4b38b1921a8d183513ce60c27beb3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/public/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
